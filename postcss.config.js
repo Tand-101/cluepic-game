2f3d5af9a518a79e6a2cd7844d2323394d1cc224
Tand-101 <tandrews101@hotmail.com>
2026-01-17 20:31:10 +0000

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+```
+
+**File 5: `.gitignore`**
+```
+node_modules
+dist
+.env.local
+.env.development.local
+.env.test.local
+.env.production.local
+.DS_Store
+*.log
