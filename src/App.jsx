c5398b45e4ae2675296f5c9a507648e47dd896d0
Tand-101 <tandrews101@hotmail.com>
2026-01-17 20:56:51 +0000

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+const CluepicGame = () => {
+  return (
+    <div style={{ 
+      minHeight: '100vh', 
+      display: 'flex', 
+      alignItems: 'center', 
+      justifyContent: 'center',
+      backgroundColor: '#f5f5f4'
+    }}>
+      <h1 style={{ fontSize: '3rem', fontWeight: 'bold' }}>
+        Cluepic Works! 🎮
+      </h1>
+    </div>
+  );
+};
+
+export default CluepicGame;
